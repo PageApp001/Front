fa2a8a6f9b39dc4becb95c5a9b154f71605d6087
JhonGS01 <git409705@gmail.com>
2024-09-24 10:45:36 -0500

    Update: enviroment enpointFront

--- src/app/enviroments/enviroment.ts
+++ src/app/enviroments/enviroment.ts
@@ -2,5 +2,5 @@ export const environment = {
 
     production: false,
     endpoint:'https://back-q0sn.onrender.com/api/',  //https://back-q0sn.onrender.com/api/  //http://localhost:3000/api/
-    front_endpoint: 'http:localhost:4200'  // https://front-xi-ashen.vercel.app/
+    front_endpoint: 'https://front-xi-ashen.vercel.app'  //http:localhost:4200
 }
