d461db125710eb4cf82f61feaf38362515347c83
JhonGS01 <git409705@gmail.com>
2024-10-08 10:45:26 -0500

    Update: views

--- src/app/enviroments/enviroment.ts
+++ src/app/enviroments/enviroment.ts
@@ -1,6 +1,17 @@
 export const environment = {
-    production: false,
-    endpoint: 'http://192.168.6.248:3000/api/', // Cambia localhost por la IP de tu servidor
-    front_endpoint: 'http://192.168.6.248:4200' // IP de la máquina que corre Angular
-  };
-  
+  production: false,
+  endpointImage:  'http://192.168.100.37:3000/uploads/',
+  endpoint:       'http://192.168.100.37:3000/api/',              
+  front_endpoint: 'http://192.168.100.37:4200',             
+};
+
+// export const environment = {
+//   production: false,
+//   endpointImage:  'https://192.168.100.37:3000/uploads/',
+//   endpoint:       'https://192.168.100.37:3000/api/',              
+//   front_endpoint: 'https://192.168.100.37:4200',             
+// };
+
+//192.168.100.37
+//http://192.168.6.248:4200 IP de la máquina que corre Angular
+//http://192.168.6.248:3000/api/ IP de tu servidor
